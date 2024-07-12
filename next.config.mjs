5079ff89e0b1d89cf036758e5d668b2352b5fe09
SFjapan <fs12265669@gmail.com>
2024-07-12 12:28:07 +0900

    buildが通るようにlintを修正。

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        forceSwcTransforms: true,
+      },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.externals = [...config.externals, 'jest'];
+        }
+        return config;
+    } 
+};
+
 
 export default nextConfig;
